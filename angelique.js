03e4d4940bb7ac9642fdf9577b1aec8bbc19d730
frederic andolina <fredericandolina@LAPTOP-KN2PLU4G.localdomain>
2021-12-15 16:08:50 +0100

    add Angelique page

---
+++ angelique.js
@@ -0,0 +1,27 @@
+/* Lorsque l'on clique sur un onglet
+ON RETIRE la class active de l'onglet actif
+j'ajoute la class active à l'onglet actuel
+
+On retire la class active sur le contenu actif
+j'ajoute la class  active sur le contenu correspondant à mon click */
+
+let tabs = document.querySelectorAll('.tabs a')
+
+for(let i = 0; i < tabs.length; i++){
+    tabs[i].addEventListener("click", function (e) {
+         const li = this.parentNode
+         const div = this.parentNode.parentNode.parentNode
+
+        if(li.classList.contains('active')){
+            return false
+        }
+        // ON RETIRE la class active de l'onglet actif
+        div.querySelector(".tabs .active").classList.remove("active")
+        // j'ajoute la class active à l'onglet actuel
+        li.classList.add("active")
+        // On retire la class active sur le contenu actif
+        div.querySelector(".tab-content.active").classList.remove("active")
+        // j'ajoute la class  active sur le contenu correspondant à mon click */
+        div.querySelector(this.getAttribute("href")).classList.add("active")
+    })
+}
