c192a9dcedef89ee17275a2f8ef841bb0995e62e
frederic andolina <fredericandolina@macbook-air-de-frederic.home>
2021-12-17 07:26:01 +0100

    add photos Angelique

--- angelique.js
+++ angelique.js
@@ -38,4 +38,13 @@ brun.style.display = "block";
 })
 
 
-    
+var isPalindrome = function (string) {
+    if (string == string.split('').reverse().join('')) {
+        alert(string + ' is palindrome.');
+    }
+    else {
+        alert(string + ' is not palindrome.');
+    }
+}
+
+console.log(isPalindrome(affa))
