3bf9a002b0c6a936349ec485d5871fd4627a9c7f
frederic andolina <fredericandolina@macbook-air-de-frederic.home>
2021-12-27 12:10:09 +0100

    Add Anais Travel

--- anais.js
+++ anais.js
@@ -1,9 +1,3 @@
-/* Lorsque l'on clique sur un onglet
-ON RETIRE la class active de l'onglet actif
-j'ajoute la class active à l'onglet actuel
-
-On retire la class active sur le contenu actif
-j'ajoute la class  active sur le contenu correspondant à mon click */
 
 let tabs = document.querySelectorAll('.tabs a')
 
@@ -26,15 +20,47 @@ for (let i = 0; i < tabs.length; i++) {
     })
 }
 
+const city = document.querySelector(".city")
+city.style.color = "red"
 
 
 const buttonTravel = document.querySelector("#travel")
 const buttonDriver = document.querySelector("#driver")
 const buttonCollation = document.querySelector("#collation")
+const buttonHotel = document.querySelector("#hotel")
+const buttonCovid = document.querySelector("#collier")
+const buttonBleu = document.querySelector("#covidBleu")
+const buttonRose = document.querySelector("#covidRose")
+const buttonAutre = document.querySelector("#covidAutre")
+
+
+
 
 const travel = document.querySelector(".travel")
 const driver = document.querySelector(".driver")
 const collation = document.querySelector(".collation")
+const hotel = document.querySelector(".hotel")
+const etiquette = document.querySelector(".etiquette")
+const bleu = document.querySelector(".bleu")
+const rose = document.querySelector(".rose")
+const autre = document.querySelector(".autre")
+
+
+buttonBleu.addEventListener("click", function () {
+    bleu.style.display = "block";
+})
+buttonRose.addEventListener("click", function () {
+    rose.style.display = "block";
+})
+buttonAutre.addEventListener("click", function () {
+    autre.style.display = "block";
+})
+
+
+buttonCovid.addEventListener("click", function () {
+    etiquette.style.display = "block";
+})
+
 
 
 buttonTravel.addEventListener("click", function () {
@@ -49,7 +75,9 @@ buttonCollation.addEventListener("click", function () {
     collation.style.display = "block";
 })
 
-
+buttonHotel.addEventListener("click", function () {
+    hotel.style.display = "block";
+})
 // const blond = document.querySelector(".blond")
 // const brun = document.querySelector(".brun")
 // // const blond = document.createElement
